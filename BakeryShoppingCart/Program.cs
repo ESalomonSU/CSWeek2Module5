939b61c00f4fe82633aed5c10c5f85e56e09c4eb
Esteban Salomon <esalomon6492@southwestuniversity.edu>
2021-05-22 20:31:27 -0600

    CS Week 2 Module 5

---
+++ BakeryShoppingCart/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ShoppingCart
+{
+    class program
+    {
+       static void Main()
+        {
+            SumNumers(1);
+        }
+
+        public static void SumNumers(int parameter)
+        {
+            if(parameter==251)
+            {
+                return;
+            }
+
+            Console.WriteLine(parameter);
+
+            parameter = parameter + 1;
+
+            SumNumers(parameter);
+        }
+    }
+}
